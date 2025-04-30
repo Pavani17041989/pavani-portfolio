f31cbab044b1a6af4ae739526b2c4f02eb716a2a
Pavani17041989 <pavani.ammu426@gmail.com>
2025-04-30 18:48:10 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Homeblog from './components/Homeblog'
 import About from './components/pages/About'
 import Skills from './components/pages/Skills'
 import Services from './components/pages/Services'
-import Certificates from './components/pages/Certificates'
+// import Certificates from './components/pages/Certificates'
 import Projects from './components/pages/Projects'
 import Contact from './components/pages/Contact'
 import Footer from './components/Footer'
@@ -24,7 +24,7 @@ const App = () => {
         <Route path='/about' element={<About/>}/>
         <Route path='/skills' element={<Skills/>}/>
         <Route path='/services' element={<Services/>}/>
-        <Route path='/certificates' element={<Certificates/>}/>           
+        {/* <Route path='/certificates' element={<Certificates/>}/>            */}
         <Route path='/projects' element={<Projects/>}/>
         <Route path='/contact' element={<Contact/>}/>
         
