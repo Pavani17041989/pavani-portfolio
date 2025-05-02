9c473a47068ad8ceb61575200ff73744b2756aff
Pavani17041989 <pavani.ammu426@gmail.com>
2025-05-02 14:07:46 +0530

    Add images

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,56 +1,62 @@
-import React from 'react'
+import React from 'react';
+import myImage from '../assets/images/upload/Pavani(2).jpg'; // use renamed file
 
 const Home = () => {
   return (
     <div>
-       <section className="home" id="home">
-      <div className="home-container">
-        <h3>Hello, I am</h3>
-        <h1>Pavani</h1>
-        <h1>I'm a <span>Full Stack Developer</span></h1>
-        <p>
-          Building Bridges Between Front-End and Back-End, Transforming Ideas
-          into Web Reality
-        </p>
-        <div className="socila-media-profile">
-          <a
-            href="https://www.linkedin.com/in/nasana-pavani/"
-            target="_blank"
-            className="tooltip-icon"
-            ><i className="fa-brands fa-linkedin-in"></i>
-            <span className="tooltiptext-icon">LinkedIn</span>
-          </a>
-          
-          <a
-            href="https://github.com/Pavani17041989"
-            target="_blank"
-            className="tooltip-icon"s
-            ><i className="fa-brands fa-github"></i>
-            <span className="tooltiptext-icon">GitHub</span>
-          </a>
-
-          <a
-            href="mailto:pavani.ammu426@gmail.com"
-            target="_blank"
-            className="tooltip-icon"
-          >
-            <i className="fas fa-envelope"></i>
-            <span className="tooltiptext-icon">Gmail</span>
+      <section className="home" id="home">
+        <div className="home-container">
+          <h3>Hello, I am</h3>
+          <h1>Pavani</h1>
+          <h1>I'm a <span>Full Stack Developer</span></h1>
+          <p>
+            Building Bridges Between Front-End and Back-End, Transforming Ideas
+            into Web Reality
+          </p>
+          <div className="socila-media-profile">
+            <a
+              href="https://www.linkedin.com/in/nasana-pavani/"
+              target="_blank"
+              className="tooltip-icon"
+              rel="noreferrer"
+            >
+              <i className="fa-brands fa-linkedin-in"></i>
+              <span className="tooltiptext-icon">LinkedIn</span>
+            </a>
+            <a
+              href="https://github.com/Pavani17041989"
+              target="_blank"
+              className="tooltip-icon"
+              rel="noreferrer"
+            >
+              <i className="fa-brands fa-github"></i>
+              <span className="tooltiptext-icon">GitHub</span>
+            </a>
+            <a
+              href="mailto:pavani.ammu426@gmail.com"
+              target="_blank"
+              className="tooltip-icon"
+              rel="noreferrer"
+            >
+              <i className="fas fa-envelope"></i>
+              <span className="tooltiptext-icon">Gmail</span>
+            </a>
+          </div>
+          <a href="Pavani_Fullstack_Developer.pdf" download className="button">
+            Download Resume <i className="fa-solid fa-download"></i>
           </a>
         </div>
-        <a href="Pavani_Fullstack_Developer.pdf" download className="button"
-          >Download Resume <i className="fa-solid fa-download"></i
-        ></a>
-      </div>
-      <div className="home-img" style={{ flex: 1, display: 'flex', justifyContent: 'center' }}>
-        <img
-          src="./src/assets/images/upload/v1709369514/Pavani(2).jpg"
-          alt="myImg" style={{ width: '350px', height: '350px',borderRadius:"50%",border:"5px solid #59b2f4" }}
-        />
-      </div>
-    </section>
-    </div>
-  )
-}
 
-export default Home
+        <div className="home-img" style={{ flex: 1, display: 'flex', justifyContent: 'center' }}>
+          <img
+            src={myImage}
+            alt="Pavani"
+            style={{ width: '350px', height: '350px', borderRadius: "50%", border: "5px solid #59b2f4" }}
+          />
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Home;
